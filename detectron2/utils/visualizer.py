8709e0555d76a50cac66a94d8f418f3117c222e5
Andy Malinsky <apmalinsky@gmail.com>
2024-11-30 10:48:14 -0500

    Update visualizer.py

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -505,16 +505,24 @@ class Visualizer:
         # draw mask for all semantic segments first i.e. "stuff"
         for mask, sinfo in pred.semantic_masks():
             category_idx = sinfo["category_id"]
-            try:
-                mask_color = [x / 255 for x in self.metadata.stuff_colors[category_idx]]
-            except AttributeError:
-                mask_color = None
-
-            # text = self.metadata.stuff_classes[category_idx]
-            text = 'stuff'
             stuff_class = [stuff for stuff in self.metadata['stuff_classes'] if stuff.get('id')==category_idx]
+            
+            text = 'stuff'
+            mask_color = None
+            
             if len(stuff_class) > 0:
                 text = stuff_class[0]['name']
+                mask_color = stuff_class[0]['color']
+                
+            # try:
+            #     # mask_color = [x / 255 for x in self.metadata.stuff_colors[category_idx]]
+            #     mask_color = color
+
+            # except AttributeError:
+            #     mask_color = None
+
+            # text = self.metadata.stuff_classes[category_idx]
+            
             self.draw_binary_mask(
                 mask,
                 color=mask_color,
